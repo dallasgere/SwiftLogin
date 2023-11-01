24051c92eaa0f8d8c472a58d6aa8f511a048999d
dallas <dallasmgere@gmail.com>
2023-11-01 18:48:31 -0500

    committing code

---
+++ SwiftLogin/SwiftLogin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftLogin
+//
+//  Created by Dallas Gere on 11/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftLogin/SwiftLogin/SwiftLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLoginApp.swift
+//  SwiftLogin
+//
+//  Created by Dallas Gere on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
